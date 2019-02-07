459e955a55609e0576ebc92204edfb1fc4af1925
mvulpe <mihai.vulpe@ligaac.ro>
2019-02-07 15:12:13 +0200

    l01.02

--- PizzaApp/src/app/app.component.ts
+++ PizzaApp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PizzaApp';
+  pizza: any;
+
+  constructor() {
+    this.pizza = {
+      name: '4 STAGIONI',
+      price: 27.5,
+      weight: 550,
+      ingredients: 'sos rosii, mozzarella, ciuperci, salam, sunca presata, oregano, anghinare'
+    }
+  }
 }
