d70a55a0ef09ac04e02fe5e4441e65c1af2598ab
mvulpe <mihai.vulpe@ligaac.ro>
2019-02-07 16:30:41 +0200

    l02.02_final

--- PizzaApp/src/app/app.module.ts
+++ PizzaApp/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent
   ],
   imports: [
     BrowserModule
---
+++ PizzaApp/src/app/tabs/tabs.component.ts
@@ -0,0 +1,27 @@
+import {Component, Input} from "@angular/core";
+import {IPizza} from "../app.model";
+
+@Component({
+  selector: 'tabs',
+  templateUrl: 'tabs.component.html',
+  styleUrls: ['tabs.component.css']
+})
+export class TabsComponent {
+
+  @Input()
+  private pizza: IPizza;
+
+  private tab: number;
+
+  constructor() {
+    this.tab = 1;
+  }
+
+  public selectTab(tid: number) {
+    this.tab = tid;
+  }
+
+  public isSelected(tid: number): boolean {
+    return this.tab === tid;
+  }
+}
