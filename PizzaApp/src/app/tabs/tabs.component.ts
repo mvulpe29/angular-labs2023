2e8f111da350b6fe2384851e6ac99639a09daaea
mvulpe <mihai.vulpe@ligaac.ro>
2019-02-07 16:46:32 +0200

    l03.01_final

--- PizzaApp/src/app/tabs/tabs.component.ts
+++ PizzaApp/src/app/tabs/tabs.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input} from "@angular/core";
-import {IPizza} from "../app.model";
+import {IPizza, IReview} from "../app.model";
 
 @Component({
   selector: 'tabs',
@@ -12,9 +12,11 @@ export class TabsComponent {
   private pizza: IPizza;
 
   private tab: number;
+  private newReview: IReview;
 
   constructor() {
     this.tab = 1;
+    this.newReview = {};
   }
 
   public selectTab(tid: number) {
@@ -23,5 +25,11 @@ export class TabsComponent {
 
   public isSelected(tid: number): boolean {
     return this.tab === tid;
+  }
+
+  public onSubmit() {
+    this.newReview.createdOn = new Date().getMilliseconds();
+    this.pizza.reviews.push(this.newReview);
+    this.newReview = {};
   }
 }
