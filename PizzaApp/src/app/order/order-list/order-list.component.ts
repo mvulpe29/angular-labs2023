d97f598b106be38786c9fb8612baae9f93cf9efe
mvulpe <mihai.vulpe@ligaac.ro>
2019-02-08 18:32:50 +0200

    l07.03

--- PizzaApp/src/app/order/order-list/order-list.component.ts
+++ PizzaApp/src/app/order/order-list/order-list.component.ts
@@ -4,6 +4,11 @@ import {IOrderService, ORDER_SERVICE} from '../order.service';
 import {IPizzaOrder} from '../pizza-order.model';
 import {INavigationService, NAVIGATION_SERVICE} from '../../commons/navigation.service';
 import {ActivatedRoute} from '@angular/router';
+import {AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators} from '@angular/forms';
+import {IOrder} from '../order.model';
+import {IPizza} from '../pizza.model';
+import {zip} from 'rxjs';
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'order-list',
@@ -13,28 +18,64 @@ import {ActivatedRoute} from '@angular/router';
 export class OrderListComponent implements OnInit {
   customer: ICustomer;
   pizzasOrders: IPizzaOrder[];
+  orderForm: FormGroup;
+  minPrice = 40;
   private customerId: string;
-
 
   constructor(@Inject(ORDER_SERVICE) private orderService: IOrderService,
               @Inject(NAVIGATION_SERVICE) private navigationService: INavigationService,
-              private activatedRoute: ActivatedRoute) {
+              private activatedRoute: ActivatedRoute,
+              private formBuilder: FormBuilder) {
     this.customerId = this.navigationService.getQueryParam(this.activatedRoute, 'customer');
+    this.orderForm = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        name: ['', Validators.required],
+        phone: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        address: ['', Validators.required],
+      }),
+      pizzaOrders: [[], [Validators.required, this.priceValidation()]]
+    });
   }
 
   ngOnInit() {
-    this.orderService.getPizzas().subscribe(pizzas => {
-      this.pizzasOrders = pizzas.map(pizza => {
+    const pizzasOrdersObservable = this.orderService.getPizzas().pipe(map((pizzas: IPizza[]) => {
+      return pizzas.map(pizza => {
         return {
           pizza,
           quantity: 1
         };
       });
+    }));
+
+    const customerObservable = this.orderService.getCustomer(this.customerId);
+
+    zip(pizzasOrdersObservable, customerObservable, (pizzaOrders: IPizzaOrder[], customer: ICustomer) => {
+      return {
+        pizzaOrders, customer
+      };
+    }).subscribe((order: IOrder) => {
+      this.pizzasOrders = order.pizzaOrders;
+      this.orderForm.patchValue({
+        customer: order.customer
+      });
     });
 
-    this.orderService.getCustomer(this.customerId).subscribe(customer => {
-      this.customer = customer;
-    });
   }
 
+  priceValidation() {
+    return (control: AbstractControl): ValidationErrors | any => {
+      if (control.value) {
+        const totalPrice = control.value.reduce((price, pizzaOrder: IPizzaOrder) => {
+          return price + pizzaOrder.quantity * pizzaOrder.pizza.price;
+        }, 0);
+        if (totalPrice < this.minPrice) {
+          return {
+            'price-validator': true
+          };
+        }
+      }
+      return undefined;
+    };
+  }
 }
--- PizzaApp/src/app/order/order.module.ts
+++ PizzaApp/src/app/order/order.module.ts
@@ -4,14 +4,14 @@ import {CommonModule} from '@angular/common';
 import {OrderRoutingModule} from './order-routing.module';
 import {OrderListComponent} from './order-list/order-list.component';
 import {OrderServiceProvider} from './order.service';
-import {FormsModule} from '@angular/forms';
-import { PriceValidatorDirective } from './price-validator.directive';
+import {ReactiveFormsModule} from '@angular/forms';
+import {PriceValidatorDirective} from './price-validator.directive';
 
 @NgModule({
   imports: [
     CommonModule,
     OrderRoutingModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   declarations: [OrderListComponent, PriceValidatorDirective],
   providers: [OrderServiceProvider]
