aad42e74a2c8dce7ce0668b70791346ab93ae0b5
mvulpe <mihai.vulpe@ligaac.ro>
2020-04-30 13:28:34 +0300

    angular update

--- PizzaApp/src/app/pizza-list/tabs/reviews/reviews.component.ts
+++ PizzaApp/src/app/pizza-list/tabs/reviews/reviews.component.ts
@@ -8,8 +8,8 @@ import {IReview} from "../../pizza.model";
 })
 export class ReviewsComponent implements OnInit {
   @Input()
-  private reviews: Array<IReview>;
-  private newReview: IReview;
+  reviews: Array<IReview>;
+  newReview: IReview;
 
   @Output()
   private addReview = new EventEmitter<IReview>();
