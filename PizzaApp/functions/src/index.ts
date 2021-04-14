9f0fe6253b359a5ecb1a793acce873c9818a566e
mvulpe <mihaivulpe3@gmail.com>
2021-04-14 18:40:41 +0300

    firebase function for save review

---
+++ PizzaApp/functions/src/index.ts
@@ -0,0 +1,20 @@
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+import * as express from 'express'
+import {Request, Response} from 'express'
+
+admin.initializeApp()
+const app = express()
+const db = admin.database()
+
+app.post('/review/:pizzaId', (req: Request, res: Response) => {
+    const pizzaId = req.params.pizzaId
+    try{
+        db.ref('pizzas').child(pizzaId).child('reviews').push(req.body)
+        res.status(200).send('yes')
+    } catch(error) {
+        res.status(500).json(error.message)
+    }
+})
+
+exports.app = functions.https.onRequest(app)
