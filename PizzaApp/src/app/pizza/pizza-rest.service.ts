440370a96b83ade38c91f32f625c7f11d646c871
mvulpe <mihaivulpe3@gmail.com>
2021-04-14 20:10:32 +0300

    adjust navigation + url
    # Conflicts:
    #	PizzaApp/src/app/pizza/pizza-rest.service.ts

--- PizzaApp/src/app/pizza/pizza-rest.service.ts
+++ PizzaApp/src/app/pizza/pizza-rest.service.ts
@@ -7,22 +7,23 @@ import {Observable} from "rxjs";
 @Injectable()
 export class PizzaRestService implements IPizzaService {
 
-  private url: string = "https://angular-labs2021-59f10-default-rtdb.firebaseio.com/pizzas.json";
+  private url: string = "https://angular-labs2021-59f10-default-rtdb.firebaseio.com/pizzas";
+  private reviewUrl: string = "https://us-central1-angular-labs2021-59f10.cloudfunctions.net/app";
 
   constructor(private http: HttpClient) {
   }
 
-  getPizza(id:string): Observable<IPizza> {
-    return this.http.get<IPizza>(`${this.url}/${id}`);
+  getPizza(id: string): Observable<IPizza> {
+    return this.http.get<IPizza>(`${this.url}/${id}.json`);
   }
 
   getPizzas(): Observable<Array<IPizza>> {
-    return this.http.get<Array<IPizza>>(this.url);
+    return this.http.get<Array<IPizza>>(this.url + '.json');
   }
 
   addReview(pizza: IPizza, review: IReview): Observable<IPizza> {
-    const url: string = this.url + '/addReview/' + pizza._id;
-    return this.http.put<IPizza>(url, review);
+    const url: string = this.reviewUrl + '/review/' + pizza.id;
+    return this.http.post<IPizza>(url, review);
   }
 
 }
