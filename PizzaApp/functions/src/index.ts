7fdeb6fd8dd9273d875b2e8c3643e864ae880c59
mvulpe <mihaivulpe3@gmail.com>
2021-04-14 18:53:01 +0300

    cors for function

--- PizzaApp/functions/src/index.ts
+++ PizzaApp/functions/src/index.ts
@@ -6,15 +6,18 @@ import {Request, Response} from 'express'
 admin.initializeApp()
 const app = express()
 const db = admin.database()
+const cors = require('cors')({origin: true});
 
 app.post('/review/:pizzaId', (req: Request, res: Response) => {
-    const pizzaId = req.params.pizzaId
-    try{
-        db.ref('pizzas').child(pizzaId).child('reviews').push(req.body)
-        res.status(200).send('yes')
-    } catch(error) {
-        res.status(500).json(error.message)
-    }
+    cors(req, res, () => {
+        const pizzaId = req.params.pizzaId
+        try {
+            db.ref('pizzas').child(pizzaId).child('reviews').push(req.body)
+            res.status(200).send('yes')
+        } catch (error) {
+            res.status(500).json(error.message)
+        }
+    })
 })
 
 exports.app = functions.https.onRequest(app)
