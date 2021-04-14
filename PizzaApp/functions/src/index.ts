73cfd317c3af75f618fbcca665c2930f6da170d3
mvulpe <mihaivulpe3@gmail.com>
2021-04-14 19:33:50 +0300

    function update

--- PizzaApp/functions/src/index.ts
+++ PizzaApp/functions/src/index.ts
@@ -6,18 +6,31 @@ import {Request, Response} from 'express'
 admin.initializeApp()
 const app = express()
 const db = admin.database()
-const cors = require('cors')({origin: true});
+const cors = require('cors');
+app.use(cors({origin: true}));
 
 app.post('/review/:pizzaId', (req: Request, res: Response) => {
-    cors(req, res, () => {
-        const pizzaId = req.params.pizzaId
-        try {
-            db.ref('pizzas').child(pizzaId).child('reviews').push(req.body)
-            res.status(200).send('yes')
-        } catch (error) {
-            res.status(500).json(error.message)
-        }
-    })
+    const pizzaId = req.params.pizzaId
+    try {
+
+        db.ref('pizzas').child(pizzaId).child('reviews').once(
+            "value", snap => {
+                const length = Object.keys(snap.val()).length
+                db.ref('pizzas').child(pizzaId).child('reviews').child('' + length).set(req.body).then()
+
+            }
+        ).then(() => {
+            db.ref('pizzas').child(pizzaId).once(
+                "value", snap => {
+                    res.status(200).send(snap.val())
+                }
+            ).then()
+        })
+
+
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
 })
 
 exports.app = functions.https.onRequest(app)
