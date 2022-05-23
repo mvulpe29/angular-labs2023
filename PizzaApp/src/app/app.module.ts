763dc51ab8dc23edd9e4441698b75a9278dea256
George Suveti <george.suveti@irian.ro>
2022-05-23 13:19:20 +0300

    - add bootstrap grid & utilities - fix toolbar - show only reactive form

--- PizzaApp/src/app/app.module.ts
+++ PizzaApp/src/app/app.module.ts
@@ -7,19 +7,23 @@ import {CommonsModule} from './commons/commons.module';
 import {AppRoutingModule} from './app-routing.module';
 import {PreloadSelectedModulesService} from './preload-selected-modules.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    AppRoutingModule,
-    BrowserModule,
-    HttpClientModule,
-    CommonsModule,
-    BrowserAnimationsModule
-  ],
+    imports: [
+        AppRoutingModule,
+        BrowserModule,
+        HttpClientModule,
+        CommonsModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatButtonModule
+    ],
   bootstrap: [AppComponent],
   providers: [PreloadSelectedModulesService]
 })
